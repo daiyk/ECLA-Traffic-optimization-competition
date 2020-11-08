192a7526bb23aee91f98e48c7ac80692206097bf
Cheng Li <lichengchris@gmail.com>
2020-11-08 11:24:12 +0100

    Update simulation.py

--- skeleton/simulation.py
+++ skeleton/simulation.py
@@ -41,6 +41,18 @@ class Simulation:
                     self.bus_index += 1
         return dictionary_bus
 
+    # This needs to be checked
+    def Get_Onboard_Person_list(self, bus_id: str):
+        list_passengersID = traci.vehicle.getPersonIDList(bus_id)
+        list_pedestrians_onboard = []
+        for i in list_passengersID:
+            for pedestrian in self.pedestrians:
+                if i == pedestrian.id:
+                    list_pedestrians_onboard.append(pedestrian)
+                else:
+                    pass
+        return list_pedestrians_onboard
+
     def pick_up_persons(self, currentEdgePerson, step):
 
         # # todo: creaete bus, update bus list (L; M; S)
@@ -87,43 +99,75 @@ class Simulation:
                     traci.vehicle.setRoute(bus_id, [self.bus_depot_start_edge])
                     traci.vehicle.changeTarget(bus_id, shortestPath[i])
                 else:
-                    traci.vehicle.setRoute(bus_id, [shortestPath[i-1], shortestPath[i]])
+                    traci.vehicle.setRoute(bus_id, [shortestPath[i - 1], shortestPath[i]])
                     traci.vehicle.changeTarget(bus_id, shortestPath[i])
                 for p in persons:
                     traci.vehicle.setStop(vehID=bus_id, edgeID=shortestPath[i], pos=p.position_from, laneIndex=0,
                                           duration=10, flags=tc.STOP_DEFAULT)
 
+            list_pedestrians_onboard = []
+            for sp in persons_to_travel:
+                list_pedestrians_onboard.append(persons_to_travel[sp])
 
+            # main runnig part
+            last_person = list_pedestrians_onboard[-1]
+            cost_des = self.find_cost_des(self, last_person, list_pedestrians_onboard)
+
+            for i in range(0, len(list_pedestrians_onboard)):
+                list_pedestrians_onboard, person_des = self.send_passenger_leave(self, cost_des,
+                                                                                 list_pedestrians_onboard,
+                                                                                 last_person, bus_id)
+                last_person = person_des
+                list_pedestrians_onboard.remove(person_des)  # remove left passenger
+                cost_des = self.find_cost_des(self, last_person, list_pedestrians_onboard)
+
+        # todo: for each passengers in the bus, redirect the bus to send as many passangers as possible to the destinations
+        for b in self.bus:
+            if (traci.vehicle.isAtBusStop(b)):
+                personListIDs = traci.vehicle.getPersonIDList(b)
+                # TODO: map personListIDs to personList! ! !
+                personList = self.Get_Onboard_Person_list(b)
+                currentEdgeID = traci.vehicle.getRoadID(b)
+                costs = []
+                for per in personList:
+                    _, cost = self.network.getShortestPath(currentEdgeID, per.edge_to)
+                    costs.append(cost)
+                # sorted_person_index = sorted(range(len(costs)), key=lambda k: s[k])
+                next_dest_ind = costs.index(min(costs))
+                traci.vehicle.setRoute(b, [personList[next_dest_ind].edge_to])
+                traci.vehicle.setStop(vehID=b, edgeID=personList[next_dest_ind].edge_from,
+                                      pos=personList[next_dest_ind].position_from, laneIndex=0,
+                                      duration=50, flags=tc.STOP_DEFAULT)
+
+
+    def find_cost_des(self, last_person, list_pedestrians_onboard):
+        cost_des = []
+
+        for person in list_pedestrians_onboard:
+            sp, cost = self.network.getShortestPaths(last_person.edge_from, person.edge_to)
+            cost_des.append(cost)
+
+        return cost_des
+
+    def send_passenger_leave(self, cost_des, list_pedestrians_onboard, last_person, bus_id):
+        # find the next leaving point
+        passenger_des_index = sorted(range(len(cost_des)), key=lambda k: cost_des[k])
+        person_des = list_pedestrians_onboard[passenger_des_index[0]]  # find the passenger with least cose
+        des_sp, cost = self.network.getShortestPaths(last_person.edge_from, person_des.edge_to)
+
+        # set SP route
+        traci.vehicle.setRoute(bus_id, des_sp)
+        traci.vehicle.changeTarget(bus_id, person_des.edge_to)
+        traci.vehicle.setStop(vehID=bus_id, edgeID=person_des.edge_to, pos=person_des.position_to, laneIndex=0,
+                              duration=50, flags=tc.STOP_DEFAULT)
+
+        return list_pedestrians_onboard, person_des
 
         # todo: return the bus to the end_edge.
         # todo: how to manage the bus return.
+        # todo: for each passengers in the bus, redirect the bus to send as many passangers as possible to the destinations
 
-        # try:
-        #     # todo: optimize depart time?
-        #     traci.vehicle.add(vehID=bus_id, typeID=bus_type, routeID="", depart=person.depart + 2, departPos=0,
-        #                       departSpeed=0, departLane=0, personCapacity=personCapacity)
-        #     traci.vehicle.setRoute(bus_id, [self.bus_depot_start_edge])
-        #
-        #     # todo: maybe get some people on the way to person_t if possible
-        #     traci.vehicle.changeTarget(bus_id, person.edge_from)
-        #     # todo: optimize duration?
-        #     traci.vehicle.setStop(vehID=bus_id, edgeID=person.edge_from, pos=person.position_from, laneIndex=0,
-        #                           duration=50, flags=tc.STOP_DEFAULT)
-        #
-        #     shortest, cost = self.network.getShortestPaths(person.edge_from, person.edge_to)
-        #     SP = [sp.getID() for sp in shortest]
-        #     traci.vehicle.setRoute(bus_id, SP)
-        #     traci.vehicle.changeTarget(bus_id, person.edge_to)
-        #     traci.vehicle.setStop(vehID=bus_id, edgeID=person.edge_to, pos=person.position_to, laneIndex=0,
-        #                           duration=50, flags=tc.STOP_DEFAULT)
-        #
-        #     # todo: how to get another people on the run?
-        #     # todo: where the bus go, after 0 passengers.
-        #
-        # except traci.exceptions.TraCIException as err:
-        #     print("TraCIException: {0}".format(err))
-        # except:
-        #     print("Unexpected error:", sys.exc_info()[0])
+
 
     def run(self):
 
